fecbc8131c4170898737c5dc81c7eaf839101b99
Umut Uzgur <umut.uzgur@mebitech.com>
2015-05-13 19:46:23 +0300

    Fourier low pass and base base filters added

--- FinalProject/SeniorProjectAutoCorrelation.cpp
+++ FinalProject/SeniorProjectAutoCorrelation.cpp
@@ -23,14 +23,13 @@ int main(int argc, char ** argv){
     for (int y = 0; y < I.rows; ++y) {
         for (int x = 0; x < I.cols; ++x) {
             result.at<double>(y,x) = calculateAutoCorrelationReg(y,x,&I);
-
         }
-        cout << y << endl;
+        //cout << y << endl;
 
     }
 
     cout << "Finished";
-    normalize(result, result, 0, 1, NORM_MINMAX);
+    normalize(result, result, 0, 8, NORM_MINMAX);
     imshow("Input",I);
     imshow("Result", result);
     waitKey();
@@ -40,15 +39,14 @@ int main(int argc, char ** argv){
 
 double calculateAutoCorrelationReg(int y, int x, Mat *I) {
     double total = 0;
-    int value = I->at<char>(y,x);
-    for (int y1 = 0; y1 < I->rows; ++y1) {
-        const char* Ii = I->ptr<char>(y1);
-        for (int x1 = 0; x1 < I->cols; ++x1) {
-            total += sqrt(pow(Ii[x1]-value,2));
+    for (int y1 = -y; y1 < I->rows-y; ++y1) {
+        for (int x1 = -x; x1 < I->cols-x; ++x1) {
+            total += I->at<char>((y+y1)+y,(x+x1)+2*x) * I->at<char>(y1+y,x1+x);
         }
         //cout << y1 << endl;
 
     }
+    //cout << total << endl;
 
 
 
--- FinalProject/SeniorProjectFourierTransform.cpp
+++ FinalProject/SeniorProjectFourierTransform.cpp
@@ -8,67 +8,147 @@
 
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
+#include <vector>
 
 using namespace cv;
+using namespace std;
 
-int main(int argc, char ** argv)
-{
-    const char* filename = argc >=2 ? argv[1] : "Pictures/shelf1.jpg";
-    
+void swapQuadrants(Mat mat);
+
+void zeroQuadrants(Mat mat, int width, int height);
+
+void lowPassFilter(Mat planes[]);
+
+void baseBandFilter(Mat planes[]);
+
+int main(int argc, char **argv) {
+    const char *filename = argc >= 2 ? argv[1] : "Pictures/shelf1.jpg";
+
     Mat I = imread(filename, IMREAD_GRAYSCALE);
-    if( I.empty())
+    if (I.empty())
         return -1;
-    
+
     Mat padded;                            //expand input image to optimal size
-    int m = getOptimalDFTSize( I.rows );
-    int n = getOptimalDFTSize( I.cols ); // on the border add zero values
+    int m = getOptimalDFTSize(I.rows);
+    int n = getOptimalDFTSize(I.cols); // on the border add zero values
     copyMakeBorder(I, padded, 0, m - I.rows, 0, n - I.cols, BORDER_CONSTANT, Scalar::all(0));
-    
+
     Mat planes[] = {Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F)};
     Mat complexI;
     merge(planes, 2, complexI);         // Add to the expanded another plane with zero
-    
+
     dft(complexI, complexI);// this way the result may fit in the source matrix
-    
-    // compute the magnitude and switch to logarithmic scale
-    // => log(1 + sqrt(Re(DFT(I))^2 + Im(DFT(I))^2))
-    split(complexI, planes);                   // planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
+
+
+    split(complexI, planes);
+
+
+    lowPassFilter(planes);
+    //baseBandFilter(planes);
+
+
+    Mat magIClone = planes[0].clone();
+
+
     magnitude(planes[0], planes[1], planes[0]);// planes[0] = magnitude
+
     Mat magI = planes[0];
-    
+
     magI += Scalar::all(1);                    // switch to logarithmic scale
     log(magI, magI);
-    
+
     // crop the spectrum, if it has an odd number of rows or columns
     magI = magI(Rect(0, 0, magI.cols & -2, magI.rows & -2));
-    
-    // rearrange the quadrants of Fourier image  so that the origin is at the image center
-    int cx = magI.cols/2;
-    int cy = magI.rows/2;
-    
-    Mat q0(magI, Rect(0, 0, cx, cy));   // Top-Left - Create a ROI per quadrant
-    Mat q1(magI, Rect(cx, 0, cx, cy));  // Top-Right
-    Mat q2(magI, Rect(0, cy, cx, cy));  // Bottom-Left
-    Mat q3(magI, Rect(cx, cy, cx, cy)); // Bottom-Right
-    
+
+
+    swapQuadrants(magI);
+
+    normalize(magI, magI, 0, 1, NORM_MINMAX);
+    // Transform the matrix with float values into a
+    // viewable image form (float between values 0 and 1).
+
+    imshow("Input Image", I);    // Show the result
+    imshow("spectrum magnitude", magI);
+    waitKey();
+
+    vector<Mat> channels;
+    //reverse
+
+    channels.push_back(magIClone);
+    channels.push_back(planes[1]);
+
+
+    Mat temp;
+    merge(channels, temp);
+
+    cv::Mat inverseTransform;
+    cv::dft(temp, inverseTransform, cv::DFT_INVERSE | cv::DFT_REAL_OUTPUT);
+
+    normalize(inverseTransform, inverseTransform, 0, 1, NORM_MINMAX);
+
+    imshow("Reconstructed", inverseTransform);
+
+    waitKey();
+
+
+    return 0;
+}
+
+void swapQuadrants(Mat mat) {
+
+    int cx = mat.cols / 2;
+    int cy = mat.rows / 2;
+
+    Mat q0(mat, Rect(0, 0, cx, cy));   // Top-Left - Create a ROI per quadrant
+    Mat q1(mat, Rect(cx, 0, cx, cy));  // Top-Right
+    Mat q2(mat, Rect(0, cy, cx, cy));  // Bottom-Left
+    Mat q3(mat, Rect(cx, cy, cx, cy)); // Bottom-Right
+
     Mat tmp;                           // swap quadrants (Top-Left with Bottom-Right)
     q0.copyTo(tmp);
     q3.copyTo(q0);
     tmp.copyTo(q3);
-    
+
     q1.copyTo(tmp);                    // swap quadrant (Top-Right with Bottom-Left)
     q2.copyTo(q1);
     tmp.copyTo(q2);
-    
-    normalize(magI, magI, 0, 1, NORM_MINMAX); // Transform the matrix with float values into a
-    // viewable image form (float between values 0 and 1).
-    
-    imshow("Input Image"       , I   );    // Show the result
-    imshow("spectrum magnitude", magI);
-    waitKey();
+}
 
 
-    
-    return 0;
+void zeroQuadrants(Mat mat, int width, int height) {
+    double value = 0;
+
+    int cx = mat.cols / 2;
+    int cy = mat.rows / 2;
+
+    Mat q0(mat, Rect(0, 0, width, height));   // Top-Left - Create a ROI per quadrant
+    Mat q1(mat, Rect(cx * 2 - width, cy * 2 - height, width, height));  // Top-Right
+    Mat q2(mat, Rect(cx * 2 - width, 0, width, height));  // Bottom-Left
+    Mat q3(mat, Rect(0, cy * 2 - height, width, height)); // Bottom-Right
+
+    q0.setTo(value);
+    q1.setTo(value);
+    q2.setTo(value);
+    q3.setTo(value);
+
+}
+
+void lowPassFilter(Mat planes[]) {
+    zeroQuadrants(planes[0], 60, 60);
+    zeroQuadrants(planes[1], 60, 60);
+}
+
+void baseBandFilter(Mat planes[]){
+
+    swapQuadrants(planes[0]);
+    swapQuadrants(planes[1]);
+
+    circle(planes[0], Point(planes[0].cols/2,planes[0].rows/2), 150, Scalar(0), 150, 8, 0);
+    circle(planes[1], Point(planes[1].cols/2,planes[1].rows/2), 150, Scalar(0), 150, 8, 0);
+
+    swapQuadrants(planes[0]);
+    swapQuadrants(planes[1]);
+
 }
